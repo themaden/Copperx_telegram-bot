814149a27fb265690906811681d8f8dea74f248a
yasinnmaden <yasinnmaden@gmail.com>
2025-03-23 13:02:34 +0300

    Update ESLint rules to convert errors to warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': ['warn'],
+    'no-useless-catch': 'warn',
+    'no-useless-escape': 'warn'
   },
 }; 
